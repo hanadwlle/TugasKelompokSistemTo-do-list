4e4149ff20a19cfd9fb0c8b4dbc1591f78f80d82
hanadwlle <hanadwlle06@gmail.com>
2025-12-22 13:38:28 +0700

    menambah database.sql

---
+++ dashboard.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+include "config/koneksi.php";
+if (!isset($_SESSION['user_id'])) header("Location: auth/login.php");
+
+$data = mysqli_query($conn,
+    "SELECT * FROM todos WHERE user_id='{$_SESSION['user_id']}'"
+);
+?>
+<h2>Todo List</h2>
+
+<form action="task/add_task.php" method="post">
+    <input name="title">
+    <input name="desc">
+    <input type="date" name="date">
+    <button>Tambah</button>
+</form>
+
+<ul>
+<?php while ($row = mysqli_fetch_assoc($data)) : ?>
+    <li>
+        <?= $row['title'] ?>
+        <a href="task/delete_task.php?id=<?= $row['id'] ?>">Hapus</a>
+    </li>
+<?php endwhile ?>
+</ul>
+
+<a href="auth/logout.php">Logout</a>
---
+++ task/add_task.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include "../config/koneksi.php";
+
+$title = $_POST['title'];
+$desc  = $_POST['desc'];
+$date  = $_POST['date'];
+
+mysqli_query($conn,
+    "INSERT INTO todos VALUES (NULL, '{$_SESSION['user_id']}', '$title', '$desc', '$date', 'pending')"
+);
+
+header("Location: ../dashboard.php");
