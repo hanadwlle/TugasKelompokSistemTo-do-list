eb6d08955bceebe27525daae89f03ba1a3e55110
mandarinixioshin <mandarinixioshin@gmail.com>
2026-01-03 17:10:17 +0800

    Menambah Keterangan pada index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-session_start();
+session_start(); // Memulai session
 
-if (isset($_SESSION['user_id'])) {
-    header("Location: dashboard.php");
+if (isset($_SESSION['user_id'])) { // Cek apakah user sudah login
+    header("Location: dashboard.php"); // Arahkan ke dashboard
     exit;
 } else {
-    header("Location: auth/login.php");
+    header("Location: auth/login.php"); // Arahkan ke halaman login
     exit;
 }
+
