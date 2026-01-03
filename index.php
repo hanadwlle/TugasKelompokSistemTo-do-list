a2766186df4374cd8de718991dd0d6ee9cc0e2f7
Ni Putu Listya Aprianti <putuaprianti6@gmail.com>
2026-01-03 14:27:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php
 session_start();
+
 if (isset($_SESSION['user_id'])) {
     header("Location: dashboard.php");
+    exit;
+} else {
+    header("Location: auth/login.php");
+    exit;
 }
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Todo List</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <h2>Aplikasi Todo List</h2>
-        <a href="auth/login.php" class="btn login">Login</a>
-        <a href="auth/register.php" class="btn register">Register</a>
-    </div>
-</body>
