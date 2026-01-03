163c265d15b6f4784bbfe1216d03572c7b702e5e
mandarinixioshin <mandarinixioshin@gmail.com>
2026-01-03 16:00:29 +0800

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,11 +1,14 @@
 <?php
+// Konfigurasi database
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "todo_db";
 
+// Koneksi ke database
 $conn = mysqli_connect($host, $user, $pass, $db);
 
+// Cek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
