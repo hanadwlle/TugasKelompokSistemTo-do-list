75bb7c0273725cb83116dbab99bf562f80fbae13
mandarinixioshin <mandarinixioshin@gmail.com>
2026-01-10 19:50:58 +0800

    Update dashboard.php
    edit judul teks

--- dashboard.php
+++ dashboard.php
@@ -26,16 +26,21 @@ $data = mysqli_query(
 <body>
 <div class="container">
     <h2>To-do List</h2>
-
-    <!-- Form tambah task -->
-    <form action="task/add_task.php" method="post">
-        <input type="text" name="title" placeholder="Judul Tugas" required>
-        <textarea name="description" placeholder="Deskripsi Tugas"></textarea>
-        <div class="form-group">
-            <label for="due_date">Deadline</label>
-            <input type="date" name="due_date" id="due_date">
-        </div>
-        <button type="submit">Tambah</button>
-    </form>
-
-    <h3>Daftar Tugas</h3>
+<ul>
+    <?php while ($row = mysqli_fetch_assoc($data)) : ?>
+    <li>
+    <!-- Judul Task -->
+    <strong>
+    <?php if ($row['status'] == 'done'): ?>
+        <s><?= htmlspecialchars($row['title']) ?></s>
+    <?php else: ?>
+        <?= htmlspecialchars($row['title']) ?>
+    <?php endif; ?>
+    </strong>
+    <!-- Deskripsi Task -->
+    <?php if (!empty($row['description'])) : ?>
+        <small class="task-desc">
+            Deskripsi : <?= htmlspecialchars($row['description']) ?>
+        </small>
+    <?php endif; ?>
+   
