f85c3224089d7dbf2013b887943eeba3a728bcd7
mandarinixioshin <mandarinixioshin@gmail.com>
2026-01-10 19:47:47 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -8,3 +8,19 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: auth/login.php");
     exit;
 }
+// Ambil task berdasarkan user yang loginn
+$user_id = $_SESSION['user_id'];
+$data = mysqli_query(
+    $conn,
+    "SELECT * FROM tasks WHERE user_id='$user_id' ORDER BY id DESC"
+);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard Todo List</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css?v=<?= time() ?>">
+</head>
+<body>
