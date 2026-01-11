5db4aa7f9f232e01b46f025863e3ec88019740be
hanadwlle <141429791+hanadwlle@users.noreply.github.com>
2026-01-11 07:41:41 +0700

    memperbaiki validasi panjang password register.php

--- auth/register.php
+++ auth/register.php
@@ -39,6 +39,7 @@ if (isset($_POST['register'])) {
 <html>
 <head>
     <title>Register</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../style.css">
 </head>
 <body>
